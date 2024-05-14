8ac79ff1d2bdb45c1a8610be1bd5d9e121119fdb
Apple <apple@Apples-MacBook-Pro.local>
2024-05-14 17:57:21 -0500

    update print module

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,8 +1,8 @@
 Rails.application.config.after_initialize do
 
   items = [
-  	Spree::Admin::MainMenu::ItemBuilder.new(Spree.t(:invoices, scope: [:admin.tab]), Spree::Core::Engine.routes.url_helpers.admin_bookkeeping_documents_path(q: { template_eq: 'invoice' })).build,
-    Spree::Admin::MainMenu::ItemBuilder.new(Spree.t(:packaging_slips, scope: [:admin.tab]), Spree::Core::Engine.routes.url_helpers.admin_bookkeeping_documents_path(q: { template_eq: 'packaging_slip' })).build
+  	Spree::Admin::MainMenu::ItemBuilder.new(Spree.t(:invoices, scope: [:admin, :tab]), Spree::Core::Engine.routes.url_helpers.admin_bookkeeping_documents_path(q: { template_eq: 'invoice' })).build,
+    Spree::Admin::MainMenu::ItemBuilder.new(Spree.t(:packaging_slips, scope: [:admin, :tab]), Spree::Core::Engine.routes.url_helpers.admin_bookkeeping_documents_path(q: { template_eq: 'packaging_slip' })).build
   ]
 
   Rails.application.config.spree_backend.main_menu.add(
