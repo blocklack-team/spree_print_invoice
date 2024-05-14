cbb177fe60f531260886e2169279c755aed628af
Apple <apple@Apples-MacBook-Pro.local>
2024-05-14 16:18:24 -0500

    update print module

--- lib/spree_print_invoice.rb
+++ lib/spree_print_invoice.rb
@@ -5,7 +5,6 @@ require 'spree/print_invoice_setting'
 require 'prawn-rails'
 require 'spree_print_invoice/prawn_rails_configuration'
 require 'spree_extension'
-require 'deface'
 
 module Spree
   module PrintInvoice
