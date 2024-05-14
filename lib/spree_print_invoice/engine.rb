86b348e5afcc35df96de87ff8218593826cc15a6
Apple <apple@Apples-MacBook-Pro.local>
2024-05-14 09:49:52 -0500

    update menu

--- lib/spree_print_invoice/engine.rb
+++ lib/spree_print_invoice/engine.rb
@@ -20,6 +20,17 @@ module SpreePrintInvoice
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
     end
+    
+    Spree::Backend::Config.configure do |config|
+      config.menu_items << config.class::MenuItem.new(
+        [:documents],          # Icono del menú (debe ser un símbolo)
+        '#sidebar-documents',  # Ruta que actúa como ancla para el submenú
+        label: Spree.t(:documents, scope: [:print_invoice]), # Texto que aparecerá en el menú
+        condition: -> { can?(:admin, Spree::Order) }, # Condición para mostrar el ítem
+        icon: 'file.svg',      # Icono para el menú (debe estar en la carpeta de iconos)
+        sub_menu: 'documents_sub_menu'
+      )
+    end    
 
     config.to_prepare(&method(:activate).to_proc)
   end
