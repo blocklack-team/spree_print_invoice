c6dd31ce4798e1296fd426c27a55895cdcbdfde9
Apple <apple@Apples-MacBook-Pro.local>
2024-05-14 09:04:36 -0500

    update config

--- app/models/spree_print_invoice/configuration.rb
+++ app/models/spree_print_invoice/configuration.rb
@@ -1,5 +1,5 @@
 module SpreePrintInvoice
-  class Configuration < ::Spree::Preferences::Configuration
+  class Configuration < Spree::Preferences::Configuration
     preference :next_number,      :integer, default: nil
     preference :logo_path,        :string,  default: 'admin/logo.png'
     preference :page_size,        :string,  default: 'LETTER'
