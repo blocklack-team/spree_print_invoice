b3e0340d1788cccb248df82a928d582919ec5e22
Apple <apple@Apples-MacBook-Pro.local>
2024-05-14 17:28:08 -0500

    update print module

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,9 +1,10 @@
-items = [
-	ItemBuilder.new('invoices', admin_bookkeeping_documents_path(q: { template_eq: 'invoice' })).build,
-	ItemBuilder.new('packaging_slips', admin_bookkeeping_documents_path(q: { template_eq: 'packaging_slip' })).build
-]
-
 Rails.application.config.after_initialize do
+
+  items = [
+  	Spree::Admin::MainMenu::ItemBuilder.new('invoices', admin_bookkeeping_documents_path(q: { template_eq: 'invoice' })).build,
+    Spree::Admin::MainMenu::ItemBuilder.new('packaging_slips', admin_bookkeeping_documents_path(q: { template_eq: 'packaging_slip' })).build
+  ]
+
   Rails.application.config.spree_backend.main_menu.add(
     Spree::Admin::MainMenu::SectionBuilder.new(Spree.t(:documents, scope: [:print_invoice]), 'file.svg').
       with_admin_ability_check(Spree::Order).
