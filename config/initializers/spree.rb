3a40cf6b4684f1365b69e90b9d18419ffe153cdf
Apple <apple@Apples-MacBook-Pro.local>
2024-05-14 18:23:04 -0500

    update print module

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,14 +1,21 @@
 Rails.application.config.after_initialize do
 
   items = [
-  	Spree::Admin::MainMenu::ItemBuilder.new(Spree.t(:invoices, scope: [:admin, :tab]), Spree::Core::Engine.routes.url_helpers.admin_bookkeeping_documents_path(q: { template_eq: 'invoice' })).build,
-    Spree::Admin::MainMenu::ItemBuilder.new(Spree.t(:packaging_slips, scope: [:admin, :tab]), Spree::Core::Engine.routes.url_helpers.admin_bookkeeping_documents_path(q: { template_eq: 'packaging_slip' })).build
+  	Spree::Admin::MainMenu::ItemBuilder.new(Spree.t(:invoices, scope: [:tab]), Spree::Core::Engine.routes.url_helpers.admin_bookkeeping_documents_path(q: { template_eq: 'invoice' })).build,
+    Spree::Admin::MainMenu::ItemBuilder.new(Spree.t(:packaging_slips, scope: [:tab]), Spree::Core::Engine.routes.url_helpers.admin_bookkeeping_documents_path(q: { template_eq: 'packaging_slip' })).build
   ]
 
   Rails.application.config.spree_backend.main_menu.add(
     Spree::Admin::MainMenu::SectionBuilder.new(Spree.t(:documents, scope: [:print_invoice]), 'file.svg').
       with_admin_ability_check(Spree::Order).
       with_items(items).
+      build
+  )
+
+  Rails.application.config.spree_backend.actions[:order].add(
+    Spree::Admin::Tabs::TabBuilder.new(Spree.t(:documents, scope: [:print_invoice]), ->(resource) { Spree::Core::Engine.routes.url_helpers.admin_order_bookkeeping_documents_path(order) ).
+      with_icon_key('file.svg').
+      with_active_check.
       build
   )
 end
