90a65f82edf03688620c3bf704875f6bbf895a32
Apple <apple@Apples-MacBook-Pro.local>
2024-05-15 12:23:35 -0500

    update template

--- app/models/spree/bookkeeping_document.rb
+++ app/models/spree/bookkeeping_document.rb
@@ -111,11 +111,53 @@ module Spree
     # Assigns +@doc+ instance variable
     #
     def render_pdf
-      template = Prawn::Template.new("#{template_name}.pdf.prawn")
+      doc: self
+
+      font_style = {
+        face: Spree::PrintInvoice::Config[:font_face],
+        size: Spree::PrintInvoice::Config[:font_size]
+      }
+      
       pdf = Prawn::Document.new
-      pdf.template template
-      #pdf.text "Hello, PDF!"
-      #pdf.text "#{template_name}.pdf.prawn"
+      pdf.font_families.update(Spree::PrintInvoiceSetting.additional_fonts)
+      pdf.define_grid(columns: 5, rows: 8, gutter: 10)
+      pdf.font font_style[:face], size: font_style[:size]
+      
+      pdf.repeat(:all) do
+        #render 'spree/printables/shared/header', pdf: pdf, printable: doc
+      end
+      
+      # CONTENT
+      pdf.grid([1,0], [6,4]).bounding_box do
+      
+        # address block on first page only
+        if pdf.page_number == 1
+          #render 'spree/printables/shared/address_block', pdf: pdf, printable: doc
+        end
+      
+        pdf.move_down 10
+      
+        #render 'spree/printables/shared/invoice/items', pdf: pdf, invoice: doc
+      
+        pdf.move_down 10
+      
+        #render 'spree/printables/shared/totals', pdf: pdf, invoice: doc
+      
+        pdf.move_down 30
+      
+        pdf.text Spree::PrintInvoice::Config[:return_message], align: :right, size: font_style[:size]
+      end
+      
+      # Footer
+      if Spree::PrintInvoice::Config[:use_footer]
+        #render 'spree/printables/shared/footer', pdf: pdf
+      end
+      
+      # Page Number
+      if Spree::PrintInvoice::Config[:use_page_numbers]
+        #render 'spree/printables/shared/page_number', pdf: pdf
+      end
+      
       pdf.render
 
       #ActionController::Base.new.render_to_string(template: "#{template_name}.pdf.prawn", layout: false, assigns: { doc: self })
