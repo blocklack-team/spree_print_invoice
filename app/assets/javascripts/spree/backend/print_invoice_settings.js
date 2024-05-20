9fd5f083c33c121e699490ce33fd2f6ee9ed152b
Apple <apple@Apples-MacBook-Pro.local>
2024-05-20 13:04:35 -0500

    print all invoices

--- app/assets/javascripts/spree/backend/print_invoice_settings.js
+++ app/assets/javascripts/spree/backend/print_invoice_settings.js
@@ -6,45 +6,47 @@ $(function() {
   });
 });
 
-document.getElementById('select-all').addEventListener('change', function() {
-  var checkboxes = document.querySelectorAll('.document-checkbox');
-  checkboxes.forEach(function(checkbox) {
-    checkbox.checked = this.checked;
-  }, this);
-});
-
-
-document.getElementById('print-selected-documents').addEventListener('click', function() {
-  var selectedCheckboxes = document.querySelectorAll('.document-checkbox:checked');
-  var documentUrls = [];
-  selectedCheckboxes.forEach(function(checkbox) {
-    documentUrls.push(checkbox.getAttribute('data-url'));
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('select-all').addEventListener('change', function() {
+    var checkboxes = document.querySelectorAll('.document-checkbox');
+    checkboxes.forEach(function(checkbox) {
+      checkbox.checked = this.checked;
+    }, this);
   });
 
-  if (documentUrls.length > 0) {
-    var form = document.createElement('form');
-    form.method = 'POST';
-    form.action = '/admin/bookkeeping_documents/combine_and_print';
-    form.style.display = 'none';
 
-    var authenticityToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-    var tokenInput = document.createElement('input');
-    tokenInput.type = 'hidden';
-    tokenInput.name = 'authenticity_token';
-    tokenInput.value = authenticityToken;
-    form.appendChild(tokenInput);
-
-    documentUrls.forEach(function(url) {
-      var urlInput = document.createElement('input');
-      urlInput.type = 'hidden';
-      urlInput.name = 'document_urls[]';
-      urlInput.value = url;
-      form.appendChild(urlInput);
+  document.getElementById('print-selected-documents').addEventListener('click', function() {
+    var selectedCheckboxes = document.querySelectorAll('.document-checkbox:checked');
+    var documentUrls = [];
+    selectedCheckboxes.forEach(function(checkbox) {
+      documentUrls.push(checkbox.getAttribute('data-url'));
     });
 
-    document.body.appendChild(form);
-    form.submit();
-  } else {
-    alert('Please, select one or more documents for print.');
-  }
+    if (documentUrls.length > 0) {
+      var form = document.createElement('form');
+      form.method = 'POST';
+      form.action = '/admin/bookkeeping_documents/combine_and_print';
+      form.style.display = 'none';
+
+      var authenticityToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+      var tokenInput = document.createElement('input');
+      tokenInput.type = 'hidden';
+      tokenInput.name = 'authenticity_token';
+      tokenInput.value = authenticityToken;
+      form.appendChild(tokenInput);
+
+      documentUrls.forEach(function(url) {
+        var urlInput = document.createElement('input');
+        urlInput.type = 'hidden';
+        urlInput.name = 'document_urls[]';
+        urlInput.value = url;
+        form.appendChild(urlInput);
+      });
+
+      document.body.appendChild(form);
+      form.submit();
+    } else {
+      alert('Please, select one or more documents for print.');
+    }
+  });
 });
