a6af343d0a151e1edaddd06050dec650b1aa017a
Apple <apple@Apples-MacBook-Pro.local>
2024-05-20 12:01:10 -0500

    print all invoices

--- app/assets/javascripts/spree/backend/print_invoice_settings.js
+++ app/assets/javascripts/spree/backend/print_invoice_settings.js
@@ -5,3 +5,22 @@ $(function() {
     return storage_path_field.prop('disabled', !$(this).prop('checked'));
   });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Seleccionar/Deseleccionar todos
+  document.getElementById('select-all').addEventListener('change', function() {
+    var checkboxes = document.querySelectorAll('.document-checkbox');
+    checkboxes.forEach(function(checkbox) {
+      checkbox.checked = this.checked;
+    }, this);
+  });
+
+  // Imprimir documentos seleccionados
+  document.getElementById('print-selected-documents').addEventListener('click', function() {
+    var selectedCheckboxes = document.querySelectorAll('.document-checkbox:checked');
+    selectedCheckboxes.forEach(function(checkbox) {
+      var url = checkbox.getAttribute('data-url');
+      window.open(url, '_blank');
+    });
+  });
+});
