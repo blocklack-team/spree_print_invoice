0687c22ee801a33f51eb1a22e81b096155c9c0b6
Apple <apple@Apples-MacBook-Pro.local>
2024-05-21 17:12:48 -0500

    update total

--- app/controllers/spree/admin/bookkeeping_documents_controller.rb
+++ app/controllers/spree/admin/bookkeeping_documents_controller.rb
@@ -32,20 +32,10 @@ module Spree
         if params[:q][:template_eq].blank?
           params[:q][:template_eq] = 'invoice'
         end
-
+      
         @search = Spree::BookkeepingDocument.ransack(params[:q])
-
-        # Assuming Spree::BookkeepingDocument belongs_to :order
-        @bookkeeping_documents = @search.result.includes(:order)
-        if order_focused?
-          @bookkeeping_documents = @bookkeeping_documents.where(printable: @order)
-        end
-
-        # Apply shipment_state filter if present
-        if params[:q][:shipment_state_eq].present?
-          @bookkeeping_documents = @bookkeeping_documents.joins(:order).where(spree_orders: { shipment_state: params[:q][:shipment_state_eq] })
-        end
-
+        @bookkeeping_documents = @search.result
+        @bookkeeping_documents = @bookkeeping_documents.where(printable: @order) if order_focused?
         @bookkeeping_documents = @bookkeeping_documents.page(params[:page] || 1).per(50)
       end
 
